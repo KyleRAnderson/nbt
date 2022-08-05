01e2c005be26b4ccd17d77d25851c2f2f77613ee
Kyle Anderson <kra.aue@gmail.com>
2022-08-05 01:37:32 +0000

    Add code generation idea
    Had an idea for reducing the amount of boilerplate that users would need
    to write to use nbt, as well as make it look nicer.
    
    The idea would be to use a format similar to Rake (but in Go, and with
    our dynamic require statements), and then simply generate the required
    task types to make it work.

---
+++ doc/design/generation idea/idea.go
@@ -0,0 +1,20 @@
+package idea
+
+import "gitlab.com/kyle_anderson/nbt/pkg/nbt"
+
+/* Idea: We could generate task type declarations from functions like the following.
+The generated code would be placed in the same package as these functions so that
+even the linters would work, after generation is done. */
+
+func TaskCompileC(h nbt.Handler, source, dest string) error {
+	/* Compile C file */
+	return nil
+}
+
+func TaskLinkProgram(h nbt.Handler) error {
+	h.Require(TaskCompileC{"hello.c", "hello.o"})
+	h.Require(TaskCompileC{"main.c", "main.o"})
+	h.Wait()
+	/* Link program. */
+	return nil
+}
